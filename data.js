80c102e03d0447a2e8e39f5bfeebb4b6ff876114
Eslam-Mohamed-1892 <eslameldakhli20@gmail.com>
2026-03-18 18:04:21 +0200

    work on

---
+++ data.js
@@ -0,0 +1,16 @@
+let orignalProducts = [
+    { name: "inpone X", price: 550, brand: "Apple", img: "https://image-cdn.ubuy.com/apple-iphone-x-256gb-space-gray/400_400_100/634e40218433e81d083d1061.jpg" },
+    { name: "inpone 12", price: 650, brand: "Apple", img: "https://i.ebayimg.com/images/g/yAMAAOSwscxmqqkL/s-l1600.webp" },
+    { name: "inpone 13", price: 700, brand: "Apple", img: "https://m.media-amazon.com/images/I/41Oa0n1UdFL._AC_SY300_SX300_QL70_ML2_.jpg" },
+    { name: "inpone 14", price: 750, brand: "Apple", img: "https://m.media-amazon.com/images/I/71oWrfCTiqL._AC_SX342_SY445_QL70_ML2_.jpg" },
+    { name: "samsung s 26 ultra", price: 1100, brand: "Samsung", img: "https://i.ebayimg.com/images/g/9VgAAeSw~1RpnWo~/s-l960.webp" },
+    { name: "samsung s 25 ultra", price: 1000, brand: "Samsung", img: "https://i.ebayimg.com/images/g/HOkAAeSw1L1pecCd/s-l960.webp" },
+    { name: "samsung s 23 ultra", price: 800, brand: "Samsung", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR60uI54GPHhFGPaZRBOrE-_tUSl9h0T5NuTA&s" },
+    { name: "oppo reno 15", price: 450, brand: "Oppo", img: "https://assets-dubaiphone.dubaiphone.net/dp-prod/wp-content/uploads/2026/01/Pre-Order-Oppo-Reno-15-Dual-Sim-256GB-12GB-Ram5G-_6988_1.webp" },
+    { name: "oppo reno 14", price: 400, brand: "Oppo", img: "https://i.ebayimg.com/images/g/1hUAAeSwzoJo8MPh/s-l1600.webp" },
+    { name: "oppo reno 13", price: 350, brand: "Oppo", img: "https://m.media-amazon.com/images/I/615ExCV5w+L._AC_SY300_SX300_QL70_ML2_.jpg" }
+]
+
+let products = orignalProducts;
+
+let cart = []
---
+++ index.js
@@ -0,0 +1,112 @@
+let appear = false
+// let showCart = () => {
+//     if (appear == false) {
+//         appear = true
+//         showYourCart()
+//     }
+//     else {
+//         appear = false
+//         tbody.innerHTML = ""
+//     }
+
+// }
+
+
+
+
+let tbody = document.querySelector("table tbody")
+let productsDiv = document.querySelector("#productsDiv")
+let table = document.querySelector("table tbody")
+let totalSpan = document.querySelector("#totalSpan")
+
+let showProducts = () => {
+    productsDiv.innerHTML = ""
+    products.forEach((el, index) => {
+        productsDiv.innerHTML += `
+                <div class="col-4 border-0">
+                    <div class="card col-12 p-3 fw-semibold">
+                        <img src="${el.img}" height="200" class="card-img-top object-fit-contain">
+                        <div class="card-body">
+                            <h5 class="card-title">${el.name}</h5>
+                            <p>${el.price} $</p>
+                            <button onclick="addToCart(${index})" class="btn btn-primary">Add To Cart</button>
+                        </div>
+                    </div>
+                </div>
+
+        `
+    })
+}
+showProducts()
+
+let caclTotal = () => {
+    let total = 0;
+    cart.forEach((el, index) => {
+        total = total + (el.qty * el.price)
+    })
+    totalSpan.textContent = total
+}
+
+let showCart = () => {
+    tbody.innerHTML = ""
+    cart.forEach((el, index) => {
+        tbody.innerHTML += `
+                    <tr class= "text-center">
+                        <td>${index + 1}</td>
+                        <td>${el.name}</td>
+                        <td>${el.price}</td>
+                        <td>
+                            <div class="d-flex align-items-center justfy-content-center gap-3">
+                                <button onclick="decrement(${index})" class="btn btn-danger">-</button>
+                                <p class="mb-0">${el.qty}</p>
+                                <button onclick="increment(${index})" class="btn btn-warning">+</button>
+                            </div>
+                        </td>
+                        <td>${el.qty * el.price}</td>
+                    </tr>
+        `
+    })
+    caclTotal()
+}
+
+let addToCart = (index) => {
+    let product = products[index]
+    let productInCart = cart.findIndex((el, index) => {
+        return el.name == product.name
+    })
+
+    if (productInCart == -1) {
+        product.qty = 1
+        cart.push(product)
+    }else{
+        cart[productInCart].qty++;
+    }
+
+    showCart()
+}
+
+let increment = (index) => {
+    cart[index].qty++;
+    showCart()
+}
+
+let decrement = (index) => {
+    cart[index].qty--;
+    if (cart[index].qty < 1) {
+        cart.splice(index, 1)
+    }
+    showCart()
+}
+
+let filterPorducts = (brand) => {
+    if (brand == "All") {
+        products = orignalProducts
+    }
+    else {
+        let final = orignalProducts.filter((el) => {
+            return el.brand == brand;
+        })
+        products = final
+    }
+    showProducts()
+}
