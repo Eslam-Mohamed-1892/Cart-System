ee9e0c3e3d8e9bf7a23f115d0f93ce2709e74c74
Eslam-Mohamed-1892 <eslameldakhli20@gmail.com>
2026-03-23 14:07:59 +0200

    new updates

--- admin.js
+++ admin.js
@@ -1,4 +1,15 @@
 let table = document.querySelector("table tbody")
+let nameInput = document.querySelector(".nameInput")
+let priceInput = document.querySelector(".priceInput")
+let brandInput = document.querySelector(".brandInput")
+let imgUrlInput = document.querySelector(".imgUrlInput")
+let editNameInput = document.querySelector(".editNameInput")
+let editPriceInput = document.querySelector(".editPriceInput")
+let editBrandInput = document.querySelector(".editBrandInput")
+let editImgUrlInput = document.querySelector(".editImgUrlInput")
+let idexOfEdit = false
+
+
 
 let showProducts = () => {
     table.innerHTML = ""
@@ -12,7 +23,7 @@ let showProducts = () => {
             <img height="50" width="50" class="rounded-circle object-fit-cover" src="${el.img}"/>
             </td>
             <td>
-                <button onclick="editItem(${index})" class="btn btn-warning" >Edit</button>
+                <button onclick="editItem(${index})" data-bs-toggle="modal" data-bs-target="#exampleModal" class="btn btn-warning" >Edit</button>
                 <button onclick="removeItem(${index})" class="btn btn-danger" >Remove</button>
             </td>
         </tr>
@@ -21,8 +32,53 @@ let showProducts = () => {
 }
 showProducts()
 
-let removeItem = (index) =>{
-    orignalProducts.splice(index , 1)
+let removeItem = (index) => {
+    orignalProducts.splice(index, 1)
+    showProducts()
+    let removeProductJSON = JSON.stringify(orignalProducts)
+    localStorage.setItem("Products", removeProductJSON)
+}
+
+
+let addNewProduct = () => {
+    let newProduct =
+    {
+        name: nameInput.value
+        , price: +priceInput.value
+        , brand: brandInput.value
+        , img: imgUrlInput.value
+    }
+    orignalProducts.push(newProduct)
+    let newProductJSON = JSON.stringify(orignalProducts)
+    localStorage.setItem('Products', newProductJSON)
+
+
+    nameInput.value = ""
+    priceInput.value = ""
+    brandInput.value = ""
+    imgUrlInput.value = ""
+
     showProducts()
 }
 
+let editItem = (index) => {
+    let product = orignalProducts[index]
+    editNameInput.value = product.name
+    editPriceInput.value = product.price
+    editBrandInput.value = product.brand
+    editImgUrlInput.value = product.img
+    idexOfEdit = index
+    let editItemJSON = JSON.stringify(orignalProducts)
+    localStorage.setItem("Products" , editItemJSON)
+}
+
+let saveChanges = () => {
+    orignalProducts[idexOfEdit].name = editNameInput.value
+    orignalProducts[idexOfEdit].price = editPriceInput.value
+    orignalProducts[idexOfEdit].brand = editBrandInput.value
+    orignalProducts[idexOfEdit].img = editImgUrlInput.value
+    alert("Saved successfully ✅");
+    showProducts()
+    let saveChangesJSON = JSON.stringify(orignalProducts)
+    localStorage.setItem("Products" , saveChangesJSON)
+}
